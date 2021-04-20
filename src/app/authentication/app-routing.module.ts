496657a38ae10a190e3ed24a7c7b4bcced9fdf2e
Angie <angelicatoro88@gmail.com>
2021-04-20 18:03:36 -0400

    Update files

---
+++ src/app/authentication/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, Router } from "@angular/router";
+
+import { PostListComponent } from '../posts/post-list/post-list.component';
+import { PostCreateComponent } from '../posts/post-create/post-create.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth.guard';
+
+
+//se maneja globalmente, por eso n se necesita hacer export explicito de los componentes declarados
+const routes: Routes = [
+
+  {
+    path: '', //main
+    component: PostListComponent
+  },
+  {
+    path: 'create', //http://localhost:4200/create
+    component:  PostCreateComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'update/:postId', //http://localhost:4200/update/345435q3
+    component: PostCreateComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',  //http://localhost:4200/login
+    component: LoginComponent
+  },
+  {
+    path: 'signup', //http://localhost:4200/signup
+    component: SignupComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    AuthGuard
+  ]
+})
+
+export class AppRoutingModule {}
